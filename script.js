810b9bd1b2e939af5b02ff0176c183a39f9af5bd
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-25 21:27:47 -0400

    bug fixes

--- script.js
+++ script.js
@@ -3,72 +3,97 @@
 var app = angular.module('app', ['ngResource']);
 
 app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,$http) {
-    $http.jsonp('http://www.reddit.com/r/jokes.json?limit=' + $scope.limit + '&jsonp=JSON_CALLBACK&subreddit=jokes')
+  var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+  var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+  var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+      // At least Safari 3+: "[object HTMLElementConstructor]"
+  var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+  var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+  if (!(isChrome || isFirefox)) {
+    $scope.message = "This app is only supported by Chrome and Firefox. Use at your own discretion."
+    $scope.badBrowser = true
+  }
+  $scope.getJokes = function(limit) {
+    $http.jsonp('http://www.reddit.com/r/jokes.json?limit=100&jsonp=JSON_CALLBACK&subreddit=jokes')
       .success(function(res) {
         $scope.jokes = res.data.children
       })
-    $scope.limit = 10
-    $scope.continuous = false
-    $scope.read = function(curIdx) {
-      if (curIdx === undefined) {
-        curIdx = 1
-      }
-      var joke = $scope.jokes[curIdx].data.title + " " + $scope.jokes[curIdx].data.selftext
-      if (window.speechSynthesis !== undefined) {
-        window.speechSynthesis.cancel()
-
-        if ($scope.continuous) {
-          nativetts(joke,function(){setTimeout(function(){$scope.read(curIdx+1)},1000)})
-        } else {
-          nativetts(joke)
-        }
-
-      } else {
-        if (window.aud !== undefined) {
-          window.aud.pause()
-        }
-        if ($scope.continuous) {
-          apitts(joke,function(){setTimeout(function(){$scope.read(curIdx+1)},1000)})
-        } else {
-          apitts(joke)
-        }
-
-      }
+  }
+  $scope.getJokes(10)
+  $scope.continuous = true
+  $scope.played = []
+  $scope.read = function(curIdx,clicked) {
+  if (clicked) {
+    $scope.played = []
+    if (window.speechSynthesis !== undefined) {
+      window.speechSynthesis.cancel()
     }
-    $scope.pause = function() {
-      if (window.speechSynthesis !== undefined) {
-        window.speechSynthesis.pause()
-      } else {
-        window.aud.pause()
-      }
+  }
+  if ($scope.played.some(function(cur) {return cur === curIdx})) {
+    return
+  } else {
+    $scope.played.push(curIdx)
+  }
+
+
+  $scope.jokes[curIdx].jokeClass="alert alert-success"
+  var joke = $scope.jokes[curIdx].data.title + " " + $scope.jokes[curIdx].data.selftext
+  if (window.speechSynthesis !== undefined) {
+
+
+    if ($scope.continuous) {
+
+      nativetts(joke,function(){
+        setTimeout(function(){
+          $scope.read(curIdx+1)
+          $scope.jokes[curIdx].jokeClass="text-warning"
+          $scope.$apply()
+        },1000)
+        })
+    } else {
+      nativetts(joke)
     }
-    $scope.play = function() {
-      if (window.speechSynthesis !== undefined) {
-        window.speechSynthesis.play()
-      } else {
-        window.aud.play()
-      }
+
+  } else {
+    if (window.aud !== undefined) {
+      window.aud.pause()
     }
-    $("[name='my-checkbox']").bootstrapSwitch({
-      size:'mini'
+    if ($scope.continuous) {
+      apitts(joke,function(){setTimeout(function(){$scope.read(curIdx+1)},1000)})
+    } else {
+      apitts(joke)
+    }
+
+  }
+  }
+  $scope.pause = function() {
+    if (window.speechSynthesis !== undefined) {
+      window.speechSynthesis.pause()
+    } else {
+      window.aud.pause()
+    }
+  }
+  $("[name='my-checkbox']").bootstrapSwitch({
+    size:'mini'
+  })
+  .on('switchChange.bootstrapSwitch', function(event, state) {
+    $scope.$apply($scope.continuous = state)
+  })
+  function nativetts(txt,cb) {
+    var arr = [];
+    var element = this;
+    while (txt.length > 0) {
+        arr.push(txt.match(pattRegex)[0]);
+        txt = txt.substring(arr[arr.length - 1].length);
+    }
+    $.each(arr, function () {
+        var u = new SpeechSynthesisUtterance(this.trim())
+        u.onend = cb
+        window.speechSynthesis.speak(u)
     })
-    .on('switchChange.bootstrapSwitch', function(event, state) {
-      $scope.$apply($scope.continuous = state)
-    })
-    function nativetts(txt,cb) {
-      var arr = [];
-      var element = this;
-      while (txt.length > 0) {
-          arr.push(txt.match(pattRegex)[0]);
-          txt = txt.substring(arr[arr.length - 1].length);
-      }
-      $.each(arr, function () {
-          var u = new SpeechSynthesisUtterance(this.trim());
-          window.speechSynthesis.speak(u);
-          u.onend = cb
-      })
 
-    }
+  }
 }])
 })(window.angular);
 var chunkLength = 150;
