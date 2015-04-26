2555d95426a91d7679947b28388cf518162d640a
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-25 23:05:36 -0400

    ie

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ var u = new SpeechSynthesisUtterance()
 u.voice = u.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Alex'; })[0];
 
 function apitts(txt,cb) {
-  window.aud.pause()
+  if (window.aud !== undefined) {window.aud.pause()}
   window.aud = new Audio("http://tts-api.com/tts.mp3?q=" + encodeURIComponent(txt))
   window.aud.play()
   window.aud.addEventListener('ended',cb, false)
